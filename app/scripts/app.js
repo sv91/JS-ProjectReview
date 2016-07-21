9aeb250216ae9e390bede94f381b2a5d671647a3
Alexander Vostriakov <alexander.vostriakov@epfl.ch>
2016-07-21 10:01:43 +0200

    Yeoman init

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name jsApp
+ * @description
+ * # jsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('jsApp', [
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
